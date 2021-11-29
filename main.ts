1a00ce44877035329012670f859c8ee11ffd7879
DaCodeManiaK <jla.webprojet@gmail.com>
2021-11-29 13:15:10 +0100

    Refactor main to it's simple expression

--- main.ts
+++ main.ts
@@ -1,25 +1,7 @@
 class Main {
-    /**
-     * Attributs : caractéristiques associées à une classe
-     */
-    whoami: string = 'Je suis un Main';
-
-    /**
-     * Comportements : la façon dont se comporte un objet de la classe
-     */
-    constructor() {
-        this.whoami = 'Something else, but in the class';
-        console.log('Hello, je suis le constructeur de Main');
+    public constructor() {
+        console.log(`I'm the Main constructor`);
     }
 }
 
-/**
- * Exécution de l'application
- */
-const myobj: Main = new Main(); // Instanciation de la classe <=> Créer un objet à partir de la classe
-myobj.whoami = 'Jean-Luc';
-console.log(myobj.whoami);
-
-const otherobj: Main = new Main();
-// otherobj.whoami = 'Un autre objet Main';
-console.log(otherobj.whoami);
+new Main();
